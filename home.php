c23a3b92d6003fc5c84a6eb4afceead726500af9
AirPenn <homberto.aag@gmail.com>
2014-07-11 19:13:51 -0400

    Update home.php

--- home.php
+++ home.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>FlyHigh - [motto]</title>
+<title>FlyHigh - The Airline Sim Reaching New Hights</title>
 <meta name="description" content="Airline Simulation Game">
 <meta name="author" content="AirPenn (dbd), MonarchA300(?), FlyHigh Forums Community">
 <meta name="keywords" content="Airline,Simulation,Game,Airline Sim,Airline Simulation,Airline Simulation Game, FLyHigh">
@@ -12,7 +12,7 @@
 <body>
 <div class="header">
 
-<div class="logo-container" colspan="3">
+<div class="logo-container">
 <a href="home.php">
 <img src="http://imgur.com/C0nMtiC.png" alt="Logo">
 </a>
