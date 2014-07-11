be3381c6c30d27c27d43b2fcaeb2047760938a02
AirPenn <homberto.aag@gmail.com>
2014-07-10 20:34:55 -0400

    Create home.php

---
+++ home.php
@@ -0,0 +1,29 @@
+<!-- WIP -->
+<!DOCTYPE html>
+<html>
+<head>
+<title>FlyHigh - [motto]</title>
+<meta name="description" content="Airline Simulation Game">
+<meta name="author" content="AirPenn (dbd), MonarchA300(?), FlyHigh Forums Community">
+<meta name="keywords" content="Airline,Simulation,Game,Airline Sim,Airline Simulation,Airline Simulation Game, FLyHigh">
+<meta charset="UTF-8">
+<link rel="stylesheet" type="text/css" href="main.css">
+</head>
+<body>
+<div class="header">
+<div class="logo-container">
+<a href="home.php">
+<img src="http://i.imgur.com/R0Aer4v.png" alt="Logo">
+</a>
+</div>
+
+</div>
+
+
+
+
+
+
+
+</body>
+</html>
