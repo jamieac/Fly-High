e64ad2f013f61ccd38d789b187afe08b548fe688
AirPenn <homberto.aag@gmail.com>
2014-07-11 22:53:54 -0400

    7-11-14 Update (AirPenn)

--- home.php
+++ home.php
@@ -1,24 +1,85 @@
-<!-- WIP -->
+<<!-- WIP -->
+
 <!DOCTYPE html>
+
 <html>
+
 <head>
+
 <title>FlyHigh - The Airline Sim Reaching New Hights</title>
+
 <meta name="description" content="Airline Simulation Game">
-<meta name="author" content="AirPenn (dbd), MonarchA300(?), FlyHigh Forums Community">
+
+<meta name="author" content="AirPenn (dbd), FlyHigh Forums Community">
+
 <meta name="keywords" content="Airline,Simulation,Game,Airline Sim,Airline Simulation,Airline Simulation Game, FLyHigh">
+
 <meta charset="UTF-8">
+
 <link rel="stylesheet" type="text/css" href="main.css">
+
 </head>
+
 <body>
+
 <div class="header">
 
+
 <div class="logo-container">
+
 <a href="home.php">
+
 <img src="http://imgur.com/C0nMtiC.png" alt="Logo">
+
 </a>
-</div>
 
 </div>
+
+
+<table class="hdr-tbl">
+<tbody>
+<tr>
+<td colspan="4">
+<h1 class="motto"><em><strong>The Airline Sim Reaching New Hights!</strong></em></h1>
+</td>
+</tr>
+<tr>
+<td>
+<a href="about:blank"><h1 class="hdr-lnks">Link1</h1></a>
+</td>
+<td>
+<a href="about:blank"><h1 class="hdr-lnks">Link2</h1></a>
+</td>
+<td>
+<a href="about:blank"><h1 class="hdr-lnks">Link3</h1></a>
+</td>
+<td>
+<a href="about:blank"><h1 class="hdr-lnks">Link4</h1></a>
+</td>
+</tr>
+<tr>
+<td>
+<a href="about:blank"><h1 class="hdr-lnks">Link5</h1></a>
+</td>
+<td>
+<a href="about:blank"><h1 class="hdr-lnks">Link6</h1></a>
+</td>
+<td>
+<a href="about:blank"><h1 class="hdr-lnks">Link7</h1></a>
+</td>
+<td>
+<a href="about:blank"><h1 class="hdr-lnks">Link8</h1></a>
+</td>
+</tr>
+<tr>
+<td colspan="4">
+<marquee sroll="4" direction="left" scrolldelay="60" scrollamount="4px">Some Sort Of Scrolling Text... Maybe A News Thing...?</marquee>
+</td>
+</tr>
+</tbody>
+</table>
+</div>
+
 
 
 
@@ -30,4 +91,5 @@
 
 
 </body>
+
 </html>
