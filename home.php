d367bf9367837294e0f797e4aba26ebd9abe1e4a
AirPenn <homberto.aag@gmail.com>
2014-07-12 23:02:23 -0400

    7-12-14 Update (AirPenn)

--- home.php
+++ home.php
@@ -1,4 +1,4 @@
-<<!-- WIP -->
+<!-- WIP -->
 
 <!DOCTYPE html>
 
@@ -73,13 +73,60 @@
 </tr>
 <tr>
 <td colspan="4">
-<marquee sroll="4" direction="left" scrolldelay="60" scrollamount="4px">Some Sort Of Scrolling Text... Maybe A News Thing...?</marquee>
+<marquee sroll="4" direction="left" scrolldelay="60" scrollamount="4px">[-Scrolling Game News Bar-]</marquee>
 </td>
 </tr>
 </tbody>
 </table>
 </div>
-
+<div class="spacer-10">
+</div>
+<div class="body">
+<div class="lft-sdbr">
+<p class="sdbr-txt"><b><u>Airline:</u></b></p>
+<a href="about:blank"><p class="sdbr-txt"><b>Link 9</b></p></a>
+<a href="about:blank"><p class="sdbr-txt"><b>Link 10</b></p></a>
+<a href="about:blank"><p class="sdbr-txt"><b>Link 11</b></p></a>
+<a href="about:blank"><p class="sdbr-txt"><b>Link 12</b></p></a>
+<a href="about:blank"><p class="sdbr-txt"><b>Link 13</b></p></a>
+<a href="about:blank"><p class="sdbr-txt"><b>Link 14</b></p></a>
+<a href="about:blank"><p class="sdbr-txt"><b>Link 15</b></p></a>
+<a href="about:blank"><p class="sdbr-txt"><b>Link 16</b></p></a>
+</div>
+<div class="lgn-bx">
+<p class="lgn-bx-txt">Log In:</p>
+<div style="height:16px;width:40px;float:left;">
+</div>
+<div style="height:16px;width:40px;float:left;">
+</div>
+<form>
+<p class="lgn-bx-txt">Username: </p><input type="text" name="username" style="float:left;margin-top:16px;">
+<div style="height:16px;width:40px;float:left;">
+</div>
+<p class="lgn-bx-txt">Password: </p><input type="password" name="password"style="float:left;margin-top:16px;">
+<div style="height:16px;width:30px;float:left;">
+</div>
+<input type="submit" value="Log In" style="float:left;margin-top:16px;">
+<div style="height:16px;width:40px;float:left;">
+</div>
+<p class="lgn-bx-txt">No Account?</p>
+<div style="height:16px;width:10px;float:left;">
+</div>
+<a href="about:blank"><p class="lgn-bx-txt" style="text-decoration:underline;">Register!</p></a>
+</form>
+</div>
+<div class="rgt-sdbr">
+<p class="sdbr-txt"><b><u>Airline:</u></b></p>
+<a href="about:blank"><p class="sdbr-txt"><b>Link 17</b></p></a>
+<a href="about:blank"><p class="sdbr-txt"><b>Link 18</b></p></a>
+<a href="about:blank"><p class="sdbr-txt"><b>Link 19</b></p></a>
+<a href="about:blank"><p class="sdbr-txt"><b>Link 20</b></p></a>
+<a href="about:blank"><p class="sdbr-txt"><b>Link 21</b></p></a>
+<a href="about:blank"><p class="sdbr-txt"><b>Link 22</b></p></a>
+<a href="about:blank"><p class="sdbr-txt"><b>Link 23</b></p></a>
+<a href="about:blank"><p class="sdbr-txt"><b>Link 24</b></p></a>
+</div>
+</div>
 
 
 
